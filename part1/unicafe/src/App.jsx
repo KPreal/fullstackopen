6f18b20ecfd75ff2b906dd2647f56284edf9e415
KPreal <kpeltoniemi@outlook.com>
2025-01-30 14:10:57 +0200

    ex1.6 working

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,9 +8,21 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button onClick={() => setBad(bad + 1)} text="bad"/> 
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
+
+const Button = (props) => (
+  <button onClick={props.onClick}>{props.text}</button>
+)
 
 export default App
