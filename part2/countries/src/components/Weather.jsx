d58c5dac442428df918bc245f3b058bdb6251b03
KPreal <kpeltoniemi@outlook.com>
2025-03-16 21:42:04 +0200

    ex2.20 added weather icon

--- part2/countries/src/components/Weather.jsx
+++ part2/countries/src/components/Weather.jsx
@@ -32,6 +32,8 @@ const Weather = ({ latlng }) => {
     return (
         <div>
             <h2>Weather</h2>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                 alt='weather-icon' />
             <p>Temperature: {weather.main.temp} °C</p>
             <p>Weather: {weather.weather[0].description}</p>
             <p>Wind: {weather.wind.speed} m/s</p>
